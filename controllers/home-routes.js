506f2ab5ecb6137137499114e172834298362427
Alex Vazquez <avazque008@outlook.com>
2022-06-03 21:16:15 -0400

    homepage created

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,7 +1,41 @@
 const router = require('express').Router();
+const { User, Post, Comment} = require('../models');
 
 router.get('/', (req, res) => {
-    res.render('homepage');
+    Post.findAll({
+        attributes: [
+            'id',
+            'post_content',
+            'title',
+            'created_at'
+        ],
+        include: [
+            {
+                model: Comment,
+                attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+                include: {
+                    model: User,
+                    attributes: ['username']
+                }
+            },
+            {
+                model: User,
+                attributes: ['username']
+            }
+        ]
+    })
+        .then(dbPostData => {
+            const posts = dbPostData.map(post => post.get({ plain: true }));
+
+            res.render('homepage', {
+                posts,
+                //loggedIn: req.session.loggedIn
+            });
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        });
 });
 
 module.exports = router;
